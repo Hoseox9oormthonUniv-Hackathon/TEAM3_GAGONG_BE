d1dd6596c35c994d48da4c61e126e24bdd7dffda
h0ch1 <h0ch1@naver.com>
2024-09-28 21:37:16 +0900

    first

---
+++ src/main/java/com/example/gagong/GagongApplication.java
@@ -0,0 +1,13 @@
+package com.example.gagong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GagongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GagongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gagong/GagongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gagong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GagongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
