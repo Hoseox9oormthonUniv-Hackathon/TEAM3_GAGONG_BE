f26ff372a883384047196eba8e8925504d7d7f39
dudxo <father8475@naver.com>
2024-09-29 03:30:37 +0900

    [feat] : 초대코드 입력 비즈니즈 로직 추가

--- src/main/java/com/example/gagong/service/InviteCodeService.java
+++ src/main/java/com/example/gagong/service/InviteCodeService.java
@@ -1,5 +1,7 @@
 package com.example.gagong.service;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,6 +9,7 @@ import com.example.gagong.dto.mapper.InviteCodeMapper;
 import com.example.gagong.dto.request.ChatRoomRequest;
 import com.example.gagong.dto.request.CreateInviteCodeRequestDto;
 import com.example.gagong.dto.response.CreateInviteCodeResponseDto;
+import com.example.gagong.dto.response.EntryInviteCodeResponse;
 import com.example.gagong.entity.InviteCode;
 import com.example.gagong.entity.Member;
 import com.example.gagong.repository.InviteCodeRepository;
@@ -44,5 +47,21 @@ public class InviteCodeService {
 		} while (inviteCodeRepository.existsByCode(code));
 
 		return code;
+	}
+
+	@Transactional
+	public EntryInviteCodeResponse entryInviteCode(int code, Member member) {
+		try {
+			InviteCode inviteCode = getInviteCodeByCode(code);
+			inviteCode.updateInviteCode(member);
+			return new EntryInviteCodeResponse(true);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("초대코드가 정상적이지 않습니다.");
+		}
+	}
+
+	private InviteCode getInviteCodeByCode(int code) {
+		return inviteCodeRepository.findByCode(code)
+			.orElseThrow(() -> new NoSuchElementException("일치하는 초대코드를 찾을 수 없습니다."));
 	}
 }
