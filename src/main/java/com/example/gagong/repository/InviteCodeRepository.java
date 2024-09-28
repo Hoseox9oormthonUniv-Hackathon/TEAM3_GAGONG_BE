edd991e1b5bf4e92ee50b02c11f2e07a377532a9
dudxo <father8475@naver.com>
2024-09-29 01:52:32 +0900

    [feat] : 초대코드 Repository 추가

---
+++ src/main/java/com/example/gagong/repository/InviteCodeRepository.java
@@ -0,0 +1,12 @@
+package com.example.gagong.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.gagong.entity.InviteCode;
+
+public interface InviteCodeRepository extends JpaRepository<InviteCode, Long> {
+
+	Optional<InviteCode> findByCode(String code);
+}
