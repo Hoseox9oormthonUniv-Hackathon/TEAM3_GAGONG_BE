043c9123eba8cbc5cb2e056c2928cb4395940986
dudxo <father8475@naver.com>
2024-09-29 02:29:15 +0900

    [feat] : 기존 초대코드 존재 여부 메서드 쿼리 추가

--- src/main/java/com/example/gagong/repository/InviteCodeRepository.java
+++ src/main/java/com/example/gagong/repository/InviteCodeRepository.java
@@ -9,4 +9,6 @@ import com.example.gagong.entity.InviteCode;
 public interface InviteCodeRepository extends JpaRepository<InviteCode, Long> {
 
 	Optional<InviteCode> findByCode(int code);
+
+	boolean existsByCode(int code);
 }
