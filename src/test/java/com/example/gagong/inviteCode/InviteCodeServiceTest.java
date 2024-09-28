282714de08df1689da3c613eef14d29195e6d00f
dudxo <father8475@naver.com>
2024-09-29 03:35:50 +0900

    [test] : 초대코드 입력 단위테스트 추가

--- src/test/java/com/example/gagong/inviteCode/InviteCodeServiceTest.java
+++ src/test/java/com/example/gagong/inviteCode/InviteCodeServiceTest.java
@@ -3,18 +3,24 @@ package com.example.gagong.inviteCode;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.example.gagong.common.InviteCodeFixture;
 import com.example.gagong.common.MemberFixture;
 import com.example.gagong.dto.request.ChatRoomRequest;
 import com.example.gagong.dto.request.CreateInviteCodeRequestDto;
 import com.example.gagong.dto.response.CreateInviteCodeResponseDto;
+import com.example.gagong.dto.response.EntryInviteCodeResponse;
 import com.example.gagong.entity.InviteCode;
 import com.example.gagong.entity.Member;
 import com.example.gagong.repository.InviteCodeRepository;
@@ -48,6 +54,7 @@ public class InviteCodeServiceTest {
 
 		// then
 		assertThat(response.code()).isEqualTo(inviteCode.getCode());
+		assertThat(member.getInviteCode().getCode()).isNotNull();
 	}
 
 	@DisplayName("초대코드를 생성 후 채팅방을 생성한다.")
@@ -66,5 +73,31 @@ public class InviteCodeServiceTest {
 		// then
 		assertThat(response.code()).isEqualTo(inviteCode.getCode());
 		then(chatRoomService).should().createChatRoom(any(ChatRoomRequest.class));
+		assertThat(member.getInviteCode().getCode()).isNotNull();
+	}
+
+	@DisplayName("초대코드를 통해 입장한다.")
+	@Test
+	void entryInviteCode() {
+		// given
+		Member member1 = MemberFixture.member();
+		Member member2 = MemberFixture.member();
+		ReflectionTestUtils.setField(member1, "id", 1L);
+		ReflectionTestUtils.setField(member2, "id", 2L);
+
+		InviteCode inviteCode = InviteCodeFixture.inviteCode2(new ArrayList<>(List.of(member1)));
+		given(inviteCodeRepository.findByCode(anyInt())).willReturn(Optional.of(inviteCode));
+
+		// when
+		EntryInviteCodeResponse response =
+			inviteCodeService.entryInviteCode(inviteCode.getCode(), member2);
+
+		// then
+		assertThat(response.result()).isTrue();
+		assertThat(inviteCode.getMembers()).extracting("id")
+			.containsExactly(
+				1L,
+				2L
+			);
 	}
 }
