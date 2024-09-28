5605cb48560306a752538746e3e62a476bc915a0
dudxo <father8475@naver.com>
2024-09-29 07:20:57 +0900

    [fix] : member.role 오류 수정

--- src/main/java/com/example/gagong/dto/request/SignupRequest.java
+++ src/main/java/com/example/gagong/dto/request/SignupRequest.java
@@ -1,12 +1,10 @@
 package com.example.gagong.dto.request;
 
-import com.example.gagong.entity.Role;
-
 public record SignupRequest(
 	String loginId,
 	String nickname,
 	String password,
-	Role role
+	String role
 ) {
 
 }
--- src/main/java/com/example/gagong/entity/Member.java
+++ src/main/java/com/example/gagong/entity/Member.java
@@ -8,8 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -43,8 +41,8 @@ public class Member implements UserDetails {
 	@Column(name = "nickname", nullable = false)
 	private String nickname;
 
-	@Enumerated(EnumType.STRING)
-	private Role role;
+	@Column(name = "role", nullable = false)
+	private String role;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "chatRoom_id")
@@ -66,7 +64,6 @@ public class Member implements UserDetails {
 		this.inviteCode = inviteCode;
 	}
 
-	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return List.of();
 	}
