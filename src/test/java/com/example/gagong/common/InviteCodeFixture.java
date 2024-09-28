93942e093025f346b08bd453dfc9975f9ce44bdb
dudxo <father8475@naver.com>
2024-09-29 03:34:55 +0900

    [test] : InviteCodeFixture 메서드 추가

--- src/test/java/com/example/gagong/common/InviteCodeFixture.java
+++ src/test/java/com/example/gagong/common/InviteCodeFixture.java
@@ -1,6 +1,9 @@
 package com.example.gagong.common;
 
+import java.util.List;
+
 import com.example.gagong.entity.InviteCode;
+import com.example.gagong.entity.Member;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,13 @@ public class InviteCodeFixture {
 	public static InviteCode inviteCode() {
 		return InviteCode.builder()
 			.code(123456)
+			.build();
+	}
+
+	public static InviteCode inviteCode2(List<Member> members) {
+		return InviteCode.builder()
+			.code(123456)
+			.members(members)
 			.build();
 	}
 }
